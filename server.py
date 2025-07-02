afb7f2ced93719b52da0eef2ad2b494e8d1745af
brooklynwidz <rish.dash.000000@gmail.com>
2025-07-02 12:47:53 +0530

    updated server.py with new contents

--- server.py
+++ server.py
@@ -1,27 +1,40 @@
 import socket
 import threading
 
-ip = '192.168.31.84'
-port = 5555
-
+targethost = "192.168.31.84"
+targetport = 5555
 def main():
     server = socket.socket()
-    server.bind((ip,port))
+    server.bind((targethost,targetport))
     server.listen(5)
-    print(f'[*] Listening on {ip}: {port}')
+
+    print(f"[*] Listening on {targethost}:{targetport}")
 
     while True:
         client, address = server.accept()
-        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+        print(f"Accepted connection from {address[0]}:{address[1]}")
+        threading.Thread(target=send_msg,args=(client,)).start()
+        threading.Thread(target=recive_msg,args=(client,)).start()
 
-        clienthandler = threading.Thread(target=handle_client, args=(client,))
-        clienthandler.start()
+# thread 1 sending msg
+def send_msg(sock):
+    while True:
+        try:
+            msg = input("enter you msg to client: ")
+            sock.send(msg.encode())
+        except KeyboardInterrupt as e:
+            print(f"[!] Error sending msg : error {e}") 
+            break
 
-def handle_client(client_socket):
-    with client_socket as sock:
-        request = sock.recv(1024)
-        print(f'[*] Received: {request.decode('utf-8')}')
-        sock.send(b'ACK')
+# thread 2 recieving msg
+def recive_msg(sock):
+    while True:
+        try:
+            msg = sock.recv(1024).decode("utf-8")
+            print(f"[*] client msg: {msg}")
+        except:
+            print("[!] Error recieving msg")
+            break
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
